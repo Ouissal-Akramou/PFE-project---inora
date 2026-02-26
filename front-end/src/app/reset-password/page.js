e7ba81b7a7f004cf0faa1dd2219a8d817e82b265
Ouissal <wissou@192.168.11.139>
2026-02-26 23:44:40 +0000

    Use NEXT_PUBLIC_API_URL for reset-password fetch

--- front-end/src/app/reset-password/page.js
+++ front-end/src/app/reset-password/page.js
@@ -29,7 +29,7 @@ export default function ResetPassword() {
     setMessage('');
 
     try {
-    const res = await fetch('http://localhost:4000/api/auth/reset-password', {
+const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/reset-password`, {
         method: 'POST',
         headers: { 
           'Content-Type': 'application/json' 
