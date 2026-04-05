fe287b8b9c571813f9049bd06d8e43dbe4642669
ouissal <ouissalakramo@gmail.com>
2026-04-06 00:58:14 +0100

    final forget password

--- back-end/lib/mailer.js
+++ back-end/lib/mailer.js
@@ -1,13 +1,7 @@
-// lib/mailer.js
 import nodemailer from "nodemailer";
-import dns from 'dns';
-
-// Force IPv4 globally
-dns.setDefaultResultOrder('ipv4first');
 
 export const transporter = nodemailer.createTransport({
-    service: "gmail",
-    family: 4,  // ← ZID HADI !!!
+    service: "gmail", // or smtp provider
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
