51fa6cde5f25efdd0e0bd05850a1c964d8c5142e
ouissal <ouissalakramo@gmail.com>
2026-04-05 19:30:39 +0100

    fix: force IPv4 with both dns and family:4

--- back-end/lib/mailer.js
+++ back-end/lib/mailer.js
@@ -1,7 +1,13 @@
+// lib/mailer.js
 import nodemailer from "nodemailer";
+import dns from 'dns';
+
+// Force IPv4 globally
+dns.setDefaultResultOrder('ipv4first');
 
 export const transporter = nodemailer.createTransport({
-    service: "gmail", // or smtp provider
+    service: "gmail",
+    family: 4,  // ← ZID HADI !!!
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
